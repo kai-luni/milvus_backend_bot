02086b7eb0df2a2d974d1d2cda5fea45ae77d98c
Kai Luenstaeden <klu@phatconsulting.de>
2023-09-05 17:05:44 +0200

    stage submission finder, better prompt

--- summarize_reddit_bot.py
+++ summarize_reddit_bot.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+import logging
 import os
 import openai
 import praw
@@ -6,7 +7,7 @@ import time
 
 from rocketchat_API.rocketchat import RocketChat
 
-from test_milvus_gpt.chat_utils import call_chatgpt_api, call_chatgpt_api_user_promt_system_prompt
+from test_milvus_gpt.chat_utils import call_chatgpt_api_user_promt_system_prompt
 
 def get_env_variable(var_name):
     value = os.getenv(var_name)
@@ -27,6 +28,11 @@ def initialize_openai():
     openai.api_version = get_env_variable('OPENAI_API_VERSION')
 
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[logging.FileHandler('reddit_sum.log'), logging.StreamHandler()])
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 client_id = get_env_variable("REDDIT_CLIENT_ID")
 client_secret = get_env_variable("REDDIT_CLIENT_SECRET")
@@ -45,41 +51,53 @@ channel = 'reddit_ukraine'  # Replace with your Rocket.Chat channel name
 initialize_openai()
 
 system_prompt = """
-[TASK1] 
-    You  reddit comments (Top Level Comment), each followed by its comments (Second-Level Comment),
-    summarize the top level comment and the second-level comment together in one text each. Make this summary for all top level comments.
- [/TASK]
+[TASK1]
+You are provided with Reddit comments. For each top-level comment and its associated second-level comments, create a single summarized text. Ensure the summary captures the essence of the conversation.
 
-[TASK2]Add the score to each comment[/TASK]
+[TASK2]
+Include the score for each comment in the format "(Score: [Score])".
 
-[TASK3]Start each new comment with '>>> '[/TASK]
+[TASK3]
+Begin each new comment with '>>> '.
 
-[TONE][/TONE]
+[TONE]
+Maintain a neutral and factual tone throughout the summary.
 
-[COMPETANCIES][/COMPETANCIES]
+[COMPETENCIES]
+Ability to extract key points from a conversation and present them in a concise manner.
 
-[FORMAT][/FORMAT]
+[FORMAT]
+Follow the format provided in the example below.
 
 [EXAMPLE]
-    >>> Comment 1: Ukraine takes town verbove (Score: 25)
-    blablabla the summary of the conversation
-    Reactions to this comment:
-    kalibu said that he likes that (Score: 15)
 
-    >>> Comment 2: 5000 russian tanks destroyed (Score: 5)
-    blablabla the summary of the conversation
-    Reactions to this comment:
-    huyu cant wait for 6000 (Score: 12)
-[EXAMPLE]
+            Comment 1: Ukraine takes town verbove (Score: 25)
+            Summary of the conversation.
+            Reactions to this comment:
+            kalibu said that he likes that (Score: 15)
 
-[!!!ADDITIONAL INSTRUCTIONS!!! Begin every response with "Latest News about Ukraine". If not, I will assume you are out of character]
+            Comment 2: 5000 Russian tanks destroyed (Score: 5)
+            Summary of the conversation.
+            Reactions to this comment:
+            huyu can't wait for 6000 (Score: 12)
+            [EXAMPLE]
 
-Here comes the text: 
-"""
+[!!!ADDITIONAL INSTRUCTIONS!!!]
+Begin every response with "Latest News about Ukraine". If not, assume you are out of character.
+    """
 
 while True:
     # Get the submission by URL
-    submission = reddit.submission(url='https://www.reddit.com/r/worldnews/comments/16ado16/rworldnews_live_thread_russian_invasion_of/')
+    #submission = reddit.submission(url='https://www.reddit.com/r/worldnews/comments/16ado16/rworldnews_live_thread_russian_invasion_of/')
+
+    # Get the subreddit
+    subreddit = reddit.subreddit('worldnews')
+
+    # Find the sticky post containing the word 'Ukraine'
+    for subm in subreddit.hot(limit=5):  # Checking the top 5 hot posts should be sufficient
+        if subm.stickied and 'Ukraine' in subm.title:
+            submission = subm
+            break
 
     # Replace the "more" comments to fetch all top-level comments
     submission.comments.replace_more(limit=None)
@@ -87,31 +105,43 @@ while True:
     # Get the current time
     current_time = datetime.utcnow()
 
-    # Filter top-level comments posted in the last 120 minutes and with a score greater than or equal to 1
-    # Then sort them so that the newest comes first
-    recent_top_level_comments = sorted([comment for comment in submission.comments if current_time - datetime.utcfromtimestamp(comment.created_utc) <= timedelta(minutes=120) and comment.score >= 1], key=lambda x: x.created_utc, reverse=True)
+    # Filter top-level comments posted in the last 120 minutes 
+    recent_top_level_comments = [comment for comment in submission.comments if current_time - datetime.utcfromtimestamp(comment.created_utc) <= timedelta(minutes=120)]
+    
+    # Calculate 'score' for each top comment based on the number of second-level comments
+    for comment in recent_top_level_comments:
+        comment.score = len(comment.replies)
+
+    # Sort the top-level comments by 'score' (number of second-level comments) and take those with a score greater than or equal to 1
+    recent_top_level_comments = sorted([comment for comment in recent_top_level_comments if comment.score >= 1], key=lambda x: x.score, reverse=True)
 
     chunks = []
 
     # Send the details of the recent top-level comments and their top 3 second-level comments to Rocket.Chat
-    for top_comment in recent_top_level_comments:        
+    for i in range(len(recent_top_level_comments)):
+        print(i)
+        top_comment = recent_top_level_comments[i]
+        test = recent_top_level_comments[1].body
         human_readable_time_top = datetime.utcfromtimestamp(top_comment.created_utc).strftime('%Y-%m-%d %H:%M:%S UTC')
         chunk = f"--- Top-Level Comment:\nAuthor: {top_comment.author}\nTime: {human_readable_time_top}\nScore: {top_comment.score}\n{top_comment.body}\n---\n"        
         #rocket.chat_post_message(top_comment_message, channel=channel)
 
         # Sort the second-level comments by score, take the top 3, and filter out those with a score less than 1
-        top_replies = sorted([reply for reply in top_comment.replies if reply.score >= 1], key=lambda x: x.score, reverse=True)[:3]
+        top_replies = sorted([reply for reply in top_comment.replies if reply.score >= 1], key=lambda x: x.score, reverse=True)[:5]
 
         # Send the top 3 second-level comments for the current top-level comment to Rocket.Chat
         for reply in top_replies:
             human_readable_time_reply = datetime.utcfromtimestamp(reply.created_utc).strftime('%Y-%m-%d %H:%M:%S UTC')
             chunk = chunk + f" Second-Level Comment:\nAuthor: {reply.author}\nTime: {human_readable_time_reply}\nScore: {reply.score}\n{reply.body}\n---\n"
-            chunks.append(chunk)
-            #rocket.chat_post_message(reply_message, channel=channel)
+        print(f"append chunk with index {i}")
+        chunks.append(chunk)
+
     print(''.join(chunks))
     #response = call_chatgpt_api("------------\nsummarize the data above, its a reddit thread. List the different topics that was talked about and give preference to the higher score comments. Show the scores when you mention a comment. List all topic by importance.", [''.join(chunks)])
+    logging.info(f">>>>>> Reddit Content: {''.join(chunks)}")
     response = call_chatgpt_api_user_promt_system_prompt(''.join(chunks), system_prompt)
     response_string = response["choices"][0]["message"]["content"]
+    logging.info(f">>>>>> GPT Response: {response_string}")
     rocket.chat_post_message(response_string, channel=channel)
 
     # Sleep for 30 minutes (1800 seconds) before the next iteration
